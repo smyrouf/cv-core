a9864731a031c89213415a1d8b68d4ec86e7c272
depenguerng <depenguerng@pontedera>
2010-11-16 10:15:40 -0500

    add Tag

--- src/main/java/com/serge/cv/Cursus.java
+++ src/main/java/com/serge/cv/Cursus.java
@@ -22,8 +22,10 @@ public class Cursus  implements Identificable<Integer> {
 	private Integer id;
 
 	@ManyToMany
-	protected Set<Resume> resumes = new HashSet<Resume>();
+	private Set<Resume> resumes = new HashSet<Resume>();
 	
+	@OneToMany
+	private Set<Tag> tags = new HashSet<Tag>();
 	
 	@Basic
 	private String title;
@@ -87,7 +89,29 @@ public class Cursus  implements Identificable<Integer> {
 		return this.title;
 	}
 
-	
+	public void setTags(Set<Tag> tags) {
+		this.tags = tags;
+	}
+
+	public Set<Tag> getTags() {
+		return this.tags;
+	}
+
+
+	public void addTag(Tag tag) {
+		this.tags.add(tag);
+		if (!tag.getEntity().equals(this) ) {
+			tag.setEntity(this);
+		}
+	}
+
+
+	public void delTag(Tag tag) {
+		if (tag.getEntity() != null ) {
+			tag.setEntity(null);
+		}
+		this.tags.remove(tag);
+	}
 
 	
 }
