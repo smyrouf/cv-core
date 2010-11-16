a515992819963b05e83fe2406102107deab93f90
depenguerng <depenguerng@pontedera>
2010-11-16 11:50:24 -0500

    remove unusued library

--- src/main/java/com/serge/cv/dao/CursusHibernateDao.java
+++ src/main/java/com/serge/cv/dao/CursusHibernateDao.java
@@ -1,18 +1,10 @@
 package com.serge.cv.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.util.List;
-
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 
 import com.serge.cv.Cursus;
-import com.serge.cv.Profil;
 import com.serge.persistence.model.DaoStandardHibernate;
 
 @Transactional
